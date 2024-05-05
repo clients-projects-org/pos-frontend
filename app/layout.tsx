62396448b0219c8716e08da6e5aa5ea710dc78e9
abdur-shobur <abdurshobur.developer@gmail.com>
2024-05-05 12:23:18 +0600

    pos init

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import './globals.css';
+import { Inter as FontSans } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
+import { StoreProvider } from './StoreProvider';
+
+const fontSans = FontSans({
+	subsets: ['latin'],
+	variable: '--font-sans',
+});
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<StoreProvider>
+			<html lang="en" suppressHydrationWarning>
+				<head />
+				<body
+					className={cn(
+						'min-h-screen bg-background font-sans antialiased',
+						fontSans.variable
+					)}
+				>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</StoreProvider>
+	);
+}
