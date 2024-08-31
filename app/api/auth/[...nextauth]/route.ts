e62b1488872146f6680416262131ef4bf91ebd7d
abdur-shobur <abdurshobur.developer@gmail.com>
2024-08-31 23:27:08 +0600

    update layout

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,51 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+async function refreshAccessToken(token) {
+	console.log('Refreshing access token', token);
+	try {
+		console.log('Beaarer token', `Bearer ${token.refreshToken}`);
 
+		const response = await fetch(
+			`${process.env.API_SERVER_BASE_URL}/api/auth/refresh`,
+			{
+				headers: {
+					Authorization: `Bearer ${token.refreshToken}`,
+				},
+			}
+		);
+
+		console.log(response);
+
+		const tokens = await response.json();
+
+		console.log(tokens);
+
+		if (!response.ok) {
+			throw tokens;
+		}
+
+		/*const refreshedTokens = {
+        "access_token": "acess-token",
+        "expires_in": 2,
+        "refresh_token": "refresh-token"
+      }*/
+
+		//return token;
+
+		return {
+			...token,
+			accessToken: tokens.accessToken,
+			refreshToken: tokens.refreshToken ?? token.refreshToken, // Fall back to old refresh token
+		};
+	} catch (error) {
+		console.log(error);
+
+		return {
+			...token,
+			error: 'RefreshAccessTokenError',
+		};
+	}
+}
 const handler = NextAuth({
 	providers: [
 		CredentialsProvider({
@@ -24,12 +69,11 @@ const handler = NextAuth({
 	},
 	callbacks: {
 		async jwt({ token, user }: { token: any; user: any }) {
-			if (user) {
-				return {
-					...user,
-				};
+			console.log({ token, user });
+			if (Date.now() < token.accessTokenExpires) {
+				return token;
 			}
-			return token;
+			return refreshAccessToken(token);
 		},
 		async session({ session, token }: { session: any; token: any }) {
 			// Attach the JWT token and user details to the session
